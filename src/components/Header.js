4b2709e46b846b6188f83b33bfa621ae8985f84e
Zana Shala <zanashala2015@gmail.com>
2023-01-08 14:42:37 +0100

    added new components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import {
   MenuIcon,
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
-export default function Header() {
+function Header() {
+  const session = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
   return (
     <div>
       <header>
@@ -13,6 +20,7 @@ export default function Header() {
         <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
           <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
             <Image
+              onClick={() => router.push("/")}
               src="https://links.papareact.com/f90"
               width={150}
               height={40}
@@ -32,17 +40,23 @@ export default function Header() {
 
           {/* Right */}
           <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap ">
-            <div className="link">
-              <p>Hello Zana</p>
+            {!session ? signIn : signOut}
+            <div onClick={signIn} className="link cursor-pointer">
+              <p className="hover:underline">
+                {session ? `Hello, ${session?.user?.name}`: "Sign In"}
+              </p>
               <p className="font-extrabold md:text-sm">Account & Lists</p>
             </div>
             <div className="link">
               <p>Returns</p>
               <p className="font-extrabold md:text-sm">& Orders</p>
             </div>
-            <div className="relative link flex items-center">
+            <div
+              onClick={() => router.push("/checkout")}
+              className="relative flex items-center cursor-pointer link flex items-center"
+            >
               <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-                0
+                {items.length}
               </span>
               <ShoppingCartIcon className="h-10" />
               <p className="hidden md:inline font-extrabold md:text-sm">
@@ -72,3 +86,5 @@ export default function Header() {
     </div>
   );
 }
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBrl2aDdcx0YMuxHAxk674rG51kycOSfDU",
+    authDomain: "yt-73517.firebaseapp.com",
+    projectId: "yt-73517",
+    storageBucket: "yt-73517.appspot.com",
+    messagingSenderId: "9704599890",
+    appId: "1:9704599890:web:9e29594dcd1853aae3ca8b",
+    measurementId: "G-M7CT2R4BYC"
+  };
